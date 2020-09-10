8e36ff3d1c09b762732408b46acd89b0ca9bd8f5
Shamim-Parvez <68326887+Shamim-Parvez@users.noreply.github.com>
2020-09-10 19:46:21 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+
+$(window).on("load", function() {
+
+	$(".loader .inner").fadeOut(1500, function() {
+		$(".loader").fadeOut(750);
+	});
+
+})
+
+
+
+$(document).ready(function(){
+
+
+    wow = new WOW(
+        {
+          animateClass: 'animated',
+          offset:       100,
+          callback:     function(box) {
+            console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+          }
+        }
+      );
+      wow.init();
+      document.getElementById('moar').onclick = function() {
+        var section = document.createElement('section');
+        section.className = 'section--purple wow fadeInDown';
+        this.parentNode.insertBefore(section, this);
+      };
+
+
+
+
+
+
+
+}); 
